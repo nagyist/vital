6b2d2c9181de262b0735a3d5c9e95e59c6eac7a6
Paul Tunison <paul.tunison@kitware.com>
2016-04-06 14:49:27 -0400

    Added compile check for if std::map can contain unique_ptr types

---
+++ CMake/configcheck/std_map_unique_ptr.cxx
@@ -0,0 +1,22 @@
+#include <map>
+#include <memory>
+#include "../../../../../../../usr/include/c++/5.3.1/map"
+
+
+int main(int argc, char *argv[])
+{
+  // Test that unique_ptr types can be inserted into std::map structures
+  // i.e. known not to be supported in GCC 4.4.7
+  typedef std::unique_ptr<int> ptr_t;
+  typedef std::map< int, std::unique_ptr<int> > test_map_t;
+  test_map_t test_map;
+
+  // insert method 1
+  int* x = new int( 5 );
+  test_map[0] = ptr_t( x );
+  // insert method 2
+  int *y = new int( 100 );
+  test_map.insert( test_map_t::value_type( 1, ptr_t(y) ) );
+
+  return 0;
+}
